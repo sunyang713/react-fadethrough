e52afd21f550eba85c471dc24f1d837538a5d7c8
Jonathan <sunyang713@hotmail.com>
2016-01-22 14:56:58 -0500

    Build.

--- dist/main.js
+++ dist/main.js
@@ -52,64 +52,100 @@
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
-	var React = __webpack_require__(2);
-	var FadeThroughComponent = __webpack_require__(154);
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	var FadeThroughContainer = module.exports = React.createClass({
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
 
-	  displayName: "FadeThroughContainer",
+	var _react = __webpack_require__(2);
 
-	  propTypes: {
-	    width: React.PropTypes.number.isRequired,
-	    height: React.PropTypes.number.isRequired,
-	    delay: React.PropTypes.number.isRequired,
-	    style: React.PropTypes.shape({
-	      content: React.PropTypes.object,
-	      overlay: React.PropTypes.object
-	    })
-	  },
+	var _react2 = _interopRequireDefault(_react);
 
-	  getInitialState: function getInitialState() {
-	    return { activeIndex: 0 };
-	  },
+	var _FadeThroughComponent = __webpack_require__(154);
 
-	  componentDidMount: function componentDidMount() {
-	    this.interval = setInterval(this.next(), this.props.delay);
-	  },
+	var _FadeThroughComponent2 = _interopRequireDefault(_FadeThroughComponent);
 
-	  componentWillUnmount: function componentWillUnmount() {
-	    clearInterval(this.interval);
-	  },
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	  next: function next() {
-	    return function () {
-	      this.setState({ activeIndex: this.state.activeIndex + 1 });
-	      if (this.state.activeIndex === this.props.children.length) this.setState({ activeIndex: 0 });
-	    };
-	  },
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	  render: function render() {
-	    var style = {
-	      width: this.props.width,
-	      height: this.props.height
-	    };
-	    return React.createElement(
-	      "div",
-	      { style: style },
-	      React.createElement(
-	        FadeThroughComponent,
-	        {
-	          activeIndex: this.state.activeIndex,
-	          delay: this.props.delay
-	        },
-	        this.props.children
-	      )
-	    );
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var FadeThroughContainer = function (_React$Component) {
+	  _inherits(FadeThroughContainer, _React$Component);
+
+	  function FadeThroughContainer() {
+	    var _Object$getPrototypeO;
+
+	    var _temp, _this, _ret;
+
+	    _classCallCheck(this, FadeThroughContainer);
+
+	    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	      args[_key] = arguments[_key];
+	    }
+
+	    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(FadeThroughContainer)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.state = {
+	      activeIndex: 0
+	    }, _temp), _possibleConstructorReturn(_this, _ret);
 	  }
 
-	});
+	  _createClass(FadeThroughContainer, [{
+	    key: 'componentDidMount',
+	    value: function componentDidMount() {
+	      this.interval = setInterval(this.next(), this.props.delay);
+	    }
+	  }, {
+	    key: 'componentWillUnmount',
+	    value: function componentWillUnmount() {
+	      clearInterval(this.interval);
+	    }
+	  }, {
+	    key: 'next',
+	    value: function next() {
+	      var _this2 = this;
+
+	      return function () {
+	        _this2.setState({ activeIndex: _this2.state.activeIndex + 1 });
+	        if (_this2.state.activeIndex === _this2.props.children.length) _this2.setState({ activeIndex: 0 });
+	      };
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var style = {
+	        width: this.props.width,
+	        height: this.props.height
+	      };
+	      return _react2.default.createElement(
+	        'div',
+	        { style: style },
+	        _react2.default.createElement(
+	          _FadeThroughComponent2.default,
+	          {
+	            activeIndex: this.state.activeIndex,
+	            delay: this.props.delay
+	          },
+	          this.props.children
+	        )
+	      );
+	    }
+	  }]);
+
+	  return FadeThroughContainer;
+	}(_react2.default.Component);
+
+	FadeThroughContainer.propTypes = {
+	  width: _react2.default.PropTypes.number.isRequired,
+	  height: _react2.default.PropTypes.number.isRequired,
+	  delay: _react2.default.PropTypes.number.isRequired
+	};
+	exports.default = FadeThroughContainer;
 
 /***/ },
 /* 2 */
@@ -19026,53 +19062,80 @@
 /* 154 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
-	var React = __webpack_require__(2);
-	var FadeThroughChild = __webpack_require__(155);
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	var FadeThroughComponent = module.exports = React.createClass({
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
 
-	  displayName: "FadeThroughComponent",
+	var _react = __webpack_require__(2);
 
-	  getDefaultProps: function getDefaultProps() {
-	    return {
-	      activeIndex: 0
-	    };
-	  },
+	var _react2 = _interopRequireDefault(_react);
 
-	  render: function render() {
-	    var _this = this;
+	var _FadeThroughChild = __webpack_require__(155);
 
-	    var inactiveStyle = {
-	      position: "absolute",
-	      transition: "opacity 3s",
-	      opacity: "0"
-	    };
+	var _FadeThroughChild2 = _interopRequireDefault(_FadeThroughChild);
 
-	    var activeStyle = {
-	      position: "absolute",
-	      transition: "opacity 3s",
-	      opacity: "1"
-	    };
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	    return React.createElement(
-	      "div",
-	      null,
-	      this.props.children.map(function (child, index) {
-	        return React.createElement(
-	          FadeThroughChild,
-	          {
-	            key: "fade-through-child" + index,
-	            style: index === _this.props.activeIndex ? activeStyle : inactiveStyle
-	          },
-	          child
-	        );
-	      })
-	    );
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var FadeThroughComponent = function (_React$Component) {
+	  _inherits(FadeThroughComponent, _React$Component);
+
+	  function FadeThroughComponent() {
+	    _classCallCheck(this, FadeThroughComponent);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(FadeThroughComponent).apply(this, arguments));
 	  }
 
-	});
+	  _createClass(FadeThroughComponent, [{
+	    key: 'render',
+	    value: function render() {
+	      var _this2 = this;
+
+	      var inactiveStyle = {
+	        position: 'absolute',
+	        transition: 'opacity 3s',
+	        opacity: '0'
+	      };
+
+	      var activeStyle = {
+	        position: 'absolute',
+	        transition: 'opacity 3s',
+	        opacity: '1'
+	      };
+
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        this.props.children.map(function (child, index) {
+	          return _react2.default.createElement(
+	            _FadeThroughChild2.default,
+	            {
+	              key: 'fade-through-child' + index,
+	              style: index === _this2.props.activeIndex ? activeStyle : inactiveStyle
+	            },
+	            child
+	          );
+	        })
+	      );
+	    }
+	  }]);
+
+	  return FadeThroughComponent;
+	}(_react2.default.Component);
+
+	FadeThroughComponent.defaultProps = {
+	  activeIndex: 0
+	};
+	exports.default = FadeThroughComponent;
 
 /***/ },
 /* 155 */
