19fcd2583fb230402162680515b2eaedf50448e6
Jonathan <sunyang713@hotmail.com>
2016-01-22 15:30:46 -0500

    Change webpack output library name (remove hyphen).

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "[name].js",
-    library: "shared-components",
+    library: ['lib', '[name]'],
     libraryTarget: "var",
   },
   externals: {
